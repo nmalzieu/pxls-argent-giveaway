f850df47496f530c03775a9a3b903a6198d733a2
nmalzieu <noe.malzieu@gmail.com>
2022-12-20 17:58:55 +0100

    Meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <StarknetConfig connectors={connectors}>
       <Head>
-        <title>Pxls x Argent</title>
+        <title>ArgentX x Pxls</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link
@@ -29,6 +29,14 @@ export default function App({ Component, pageProps }: AppProps) {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
+        <meta property="og:url" content="https://argentx.pxls.wtf" />
+        <meta property="og:title" content="ArgentX x Pxls" />
+        <meta
+          property="og:image"
+          content="https://argentx.pxls.wtf/social_sharing.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Head>
       <Component {...pageProps} />
     </StarknetConfig>
